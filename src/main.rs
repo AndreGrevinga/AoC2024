74ac58997996f5149c6970d9054f62ea0acb58b1
Andre Grevinga <andre.grevinga@gmail.com>
2024-12-08 18:08:59 +0100

    Added solution for both parts of day three

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use day_two::day_two::day_two_part_two;
+use day_three::day_three::day_three_part_two;
 
 mod day_one;
+mod day_three;
 mod day_two;
 
 fn main() {
-    day_two_part_two();
+    day_three_part_two();
 }
