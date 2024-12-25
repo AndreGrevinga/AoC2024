827e7cf330989e9e320d038bb8f2d32c96c6f4b1
Andre Grevinga <andre.grevinga@gmail.com>
2024-12-25 13:07:35 +0100

    Remove unnecessary files

--- target/debug/aoc2024.d
+++
@@ -1,1 +0,0 @@
-/home/kromi/.local/programming/AoC2024/target/debug/aoc2024: /home/kromi/.local/programming/AoC2024/src/day_nine/day_nine.rs /home/kromi/.local/programming/AoC2024/src/day_nine/mod.rs /home/kromi/.local/programming/AoC2024/src/main.rs
--- target/debug/deps/aoc2024-27aa50e87583811a.d
+++
@@ -1,9 +0,0 @@
-/home/kromi/.local/programming/AoC2024/target/debug/deps/aoc2024-27aa50e87583811a: src/main.rs src/day_one/mod.rs src/day_one/day_one.rs src/day_two/mod.rs src/day_two/day_two.rs
-
-/home/kromi/.local/programming/AoC2024/target/debug/deps/aoc2024-27aa50e87583811a.d: src/main.rs src/day_one/mod.rs src/day_one/day_one.rs src/day_two/mod.rs src/day_two/day_two.rs
-
-src/main.rs:
-src/day_one/mod.rs:
-src/day_one/day_one.rs:
-src/day_two/mod.rs:
-src/day_two/day_two.rs:
